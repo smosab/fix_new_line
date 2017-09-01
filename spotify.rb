e291d05f7969cb42fddadf841b69d5e50d94b652
Mosab Sasi <smosab@gmail.com>
2017-09-01 13:44:44 -0600

    spotify api test

---
+++ spotify.rb
@@ -0,0 +1,14 @@
+require 'pry'
+require 'rspotify'
+require 'rspotify/oauth'
+
+RSpotify.authenticate("1ebbccde78c9433fae5edc58b449f070", "6846505ae3fe4e678271e8adb2eb85d6")
+binding.pry
+
+#me = RSpotify::User.find('deephousehead')
+#2M6ai8ktULjmT3iEvJuJOz
+
+
+
+
+
